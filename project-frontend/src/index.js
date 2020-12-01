700a4d2932a67ecffbe93b3d3c9240ff88a7a551
christinezosche <christine.zosche@gmail.com>
2020-12-01 14:27:55 -0800

    Added a Recipe class in JS, edited filtering to create Recipe objects and store them in an array

--- project-frontend/src/index.js
+++ project-frontend/src/index.js
@@ -1,3 +1,15 @@
+class Recipe {
+    constructor(name, ingredients, time, steps, skill_level, course) {
+      this.name = name;
+      this.ingredients = ingredients;
+      this.time = time;
+      this.steps = steps;
+      this.skill_level = skill_level;
+      this.course = course;
+    }
+  }
+
+const searchResults = []
 const RECIPES_URL = `http://localhost:3000/recipes`
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -5,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const newDiv = document.createElement('div')
     newDiv.className = 'card'
     let question = document.createElement('p')
+    question.id = "course-question"
     question.innerHTML = `What course are you making? `
     let input = document.createElement("input");
         input.type = "text"
@@ -15,29 +28,53 @@ document.addEventListener('DOMContentLoaded', () => {
     question.appendChild(input)
     question.appendChild(btn)
     newDiv.appendChild(question)
-    container.append(newDiv)
+    container.appendChild(newDiv)
 
 
 let courseButton = document.getElementById('course-button')
 let courseText = document.querySelector('input[name="course"]')
-courseButton.addEventListener('click', () => {
-    
+courseButton.addEventListener('click', findRecipes)
+
+function findRecipes () {
+    if (courseText.value == '') {
+        alert("Please enter a course");
+    }
+    else {
     fetch(RECIPES_URL)
     .then(function(response) {
         return response.json();
       })
       .then(function(array) {
         const search = courseText.value.toLowerCase()
-        let searchResults = array.filter(recipe => {
-            return recipe.course.toLowerCase().includes(search)
-            })
-        console.log(searchResults)
+        let filteredRecipes = array.filter(recipe => {
+                 return recipe.course.toLowerCase().includes(search)
+                 })
+        for (const recipe of filteredRecipes) {
+            searchResults.push(new Recipe(recipe.name, recipe.ingredients, recipe.time, recipe.steps, recipe.skill_level, recipe.course))
+          }
+        courseButton.removeEventListener('click', findRecipes)
+        let question = document.getElementById('course-question')
+        question.className = "selected"
+        // let searchResults = array.filter(recipe => {
+        //     return recipe.course.toLowerCase().includes(search)
+        //     })
+        // console.log(searchResults)
         for (const recipe of searchResults) {
             renderRecipeName(recipe)
           }
       })
+    }
+}
 
+let testButton = document.createElement("button");
+testButton.innerHTML = `Test`
+container.appendChild(testButton)
+testButton.addEventListener('click', () => {
+    for (const recipe of searchResults) {
+        console.log(recipe)
+      }
 })
+
 
 })
 
@@ -48,4 +85,3 @@ function renderRecipeName(recipe) {
     potentialResults.appendChild(item)
 }
 
-// @mountains = Mountain.where('country LIKE ?', "%#{params[:country]}%")
