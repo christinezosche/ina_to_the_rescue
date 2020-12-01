699eb42dd41c15cd320ba31681d9262b13a3e138
christinezosche <christine.zosche@gmail.com>
2020-11-30 16:06:05 -0800

    Added routes and corresponding controller actions.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+
+    def index
+        recipes = Recipe.all
+        render json: recipes, only: [:id, :name, :course, :time, :ingredients, :steps, :skill_level, ], :include => {
+            :ratings => {:only => [:value]}
+    }
+    end
+
+    def show
+        recipe = Recipe.find(params[:id])
+        render json: recipe, only: [:id, :name, :course, :time, :ingredients, :steps, :skill_level, ], :include => {
+            :ratings => {:only => [:value]}
+    }
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
 end
